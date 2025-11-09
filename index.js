e87bd1bf15977e640ecd265dc9b9bd6765d2cd30
idlakshan <id.lakshan21@gmail.com>
2025-11-09 20:13:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config();
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
+const app = express()
+app.use(cors({
+    credentials:true,
+    origin:process.env.FRONTEND_URL
+}))
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan())
+app.use(helmet({
+    crossOriginResourcePolicy:false
+}))
+
+
+const PORT = 8080 || process.env.PORT
+
+app.get("/",(req,res)=>{
+  res.json({
+    message:"Server is running"
+  })
+})
+
+app.listen(PORT,()=>{
+    console.log("Server is running "+PORT);
+    
+})
