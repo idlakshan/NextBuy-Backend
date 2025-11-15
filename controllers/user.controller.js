f45c944543167c36a3bea647bf996d39042462c9
idlakshan <id.lakshan21@gmail.com>
2025-11-15 15:12:49 +0530

    feat: add user registration and send email

---
+++ controllers/user.controller.js
@@ -0,0 +1,65 @@
+import UserModel from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import sendEmail from "../config/sendEmail.js";
+import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
+
+export async function registerUserController(req, res) {
+  try {
+     const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "provide email, name, password",
+        error: true,
+        success: false,
+      });
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (user) {
+      return res.json({
+        message: "Already register email",
+        error: true,
+        success: false,
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const payload = {
+      name,
+      email,
+      password: hashPassword,
+    };
+
+    const newUser = new UserModel(payload);
+    const save = await newUser.save();
+
+    const verifyEmailUrl = `${process.env.FRONTEND_URL}/verify-email?code=${save?._id}`;
+
+    await sendEmail({
+      sendTo: email,
+      subject: "Verify Email from NextBuy",
+      html: verifyEmailTemplate({ name, url: verifyEmailUrl }),
+    });
+
+    return res.json({
+      message: "user register successfully",
+      error: false,
+      success: true,
+      data: save,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function verifyEmail() {
+  
+}
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import helmet from "helmet";
 import connectDB from "./config/connectDB.js";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 app.use(
@@ -31,6 +32,8 @@ app.get("/", (req, res) => {
     message: "Server is running",
   });
 });
+
+app.use('/api/user',userRouter)
 
 connectDB().then(() => {
   app.listen(PORT, () => {
